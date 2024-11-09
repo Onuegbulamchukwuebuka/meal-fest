00db1420a3bacb2cbc63bd282bf02e58d5fadb84
Onuegbulam Chukwuebuka <princewillchukwuebuka5@gmail.com>
2024-11-08 20:09:43 -0800

    first commit

--- src/Componet/Offers/Offers.tsx
+++ src/Componet/Offers/Offers.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Offers = () => {
-  return <div>Offers Offers</div>;
+  return (
+    <div className="h-[70vh] bg-red-900">
+      <div></div>
+    </div>
+  );
 };
 
 export default Offers;
