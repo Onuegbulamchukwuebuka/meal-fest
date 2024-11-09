459166db3ba8b11ee03ac5d98eaace9cfa90ca56
Onuegbulam Chukwuebuka <princewillchukwuebuka5@gmail.com>
2024-11-08 20:16:55 -0800

    first commit

--- src/Componet/Offers/Offers.tsx
+++ src/Componet/Offers/Offers.tsx
@@ -3,7 +3,11 @@ import React from "react";
 const Offers = () => {
   return (
     <div className="h-[70vh] bg-red-900">
-      <div></div>
+      <div className="h-[70vh] w-[95%] ">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
