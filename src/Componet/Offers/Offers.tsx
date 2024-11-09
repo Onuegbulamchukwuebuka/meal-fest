447a6f92678ccb018b5a9f16e782e17c936e3601
Onuegbulam Chukwuebuka <princewillchukwuebuka5@gmail.com>
2024-11-08 19:54:10 -0800

    first commit

---
+++ src/Componet/Offers/Offers.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Offers = () => {
+  return <div>Offers Offers</div>;
+};
+
+export default Offers;
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NarBar from "../Componet/NavBar/NavBar";
 import HeroPage from "../Componet/HeroPage/HeroPage";
+import Offers from "../Componet/Offers/Offers";
 
 const Home = () => {
   return (
     <div>
       <NarBar />
       <HeroPage />
+      <Offers />
     </div>
   );
 };
